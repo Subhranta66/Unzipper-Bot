650ab0551c85eacd535eeb85405f338d5e31e92e
Subhranta66 <86653080+Subhranta66@users.noreply.github.com>
2021-12-22 20:44:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ class Config(object):
     MONGODB_URL = os.environ.get("MONGODB_URL")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
-    TG_MAX_SIZE = 2040108421
+    TG_MAX_SIZE = 200040108421
